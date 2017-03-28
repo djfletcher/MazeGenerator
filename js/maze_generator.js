990f113a6d218799cba66acfbcac64dd6ae2f145
Daniel <daniel.j.fletcher2@gmail.com>
2017-03-28 15:33:15 -0700

    Got maze to render as single line inside canvas

--- js/maze_generator.js
+++ js/maze_generator.js
@@ -1,13 +1,6 @@
 import { createGrid, carvePassagesFrom } from './algorithm';
-//
-// function setup() {}
-// function draw() {}
-// function beginShape() {}
-// function vertex() {}
-// function endShape() {}
-// function noFill() {}
 
-const drawMaze = grid => {
+const getCoordinates = grid => {
   const coords = { x: 30, y: 30 };
   const vertices = [[30, 20]];
 
@@ -15,64 +8,43 @@ const drawMaze = grid => {
     row.forEach(square => {
       switch(square) {
         case 'n':
-          coords.y += 10;
+          coords.y -= 100;
           vertices.push([coords.x, coords.y]);
         case 's':
-          coords.y -= 10;
+          coords.y += 100;
           vertices.push([coords.x, coords.y]);
         case 'w':
-          coords.x += 10;
+          coords.x -= 100;
           vertices.push([coords.x, coords.y]);
         case 'e':
-          coords.x -= 10;
+          coords.x += 100;
           vertices.push([coords.x, coords.y]);
       }
     });
-
-    debugger;
-    setup(vertices);
   });
+
+  return vertices;
 };
 
-
-function setup(vertices) {
-  noFill();
-  beginShape();
-  debugger;
-  vertices.forEach(v => vertex(...v));
-  endShape();
-}
-
-// function setup(vertices) {
-//   noFill();
-//   beginShape();
-//   vertex(30, 20);
-//   vertex(85, 20);
-//   vertex(85, 75);
-//   vertex(30, 75);
-//   endShape();
-// }
-
-// function draw(vertices) {
-//   // debugger;
-//   noFill();
-//   beginShape();
-//   vertices.forEach(v => vertex(...v));
-//   endShape();
-// }
+const draw = (ctx, vertices) => {
+  ctx.moveTo(...(vertices[0]));
+  ctx.beginPath();
+  vertices.forEach(vertex => ctx.lineTo(...vertex));
+  ctx.stroke();
+};
 
 document.addEventListener("DOMContentLoaded", () => {
-
-  window.setup = setup;
+  // window.setup = setup;
   // window.draw = draw;
-  // window.noFill = noFill;
-  // window.beginShape = beginShape;
-  // window.vertex = vertex;
-  //
 
-  const grid = createGrid(5, 5);
-  console.log(grid);
-  carvePassagesFrom(0, 0, grid);
-  console.log(grid);
-  drawMaze(grid);
+  const canvas = document.getElementById('canvas');
+  if (canvas.getContext) {
+    const ctx = canvas.getContext('2d');
+
+    const grid = createGrid(5, 5);
+    carvePassagesFrom(0, 0, grid);
+    const coords = getCoordinates(grid);
+
+    draw(ctx, coords);
+  }
 });
