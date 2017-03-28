1aab3868a54e1ae4cbf1cf7069f972db27aa856e
Daniel <daniel.j.fletcher2@gmail.com>
2017-03-28 13:12:39 -0700

    Experiment with using p5 to draw mmaze, not currently rendering on screen

--- js/maze_generator.js
+++ js/maze_generator.js
@@ -1,20 +1,78 @@
 import { createGrid, carvePassagesFrom } from './algorithm';
+//
+// function setup() {}
+// function draw() {}
+// function beginShape() {}
+// function vertex() {}
+// function endShape() {}
+// function noFill() {}
 
-function setup() {
-  createCanvas(600, 400);
+const drawMaze = grid => {
+  const coords = { x: 30, y: 30 };
+  const vertices = [[30, 20]];
+
+  grid.forEach(row => {
+    row.forEach(square => {
+      switch(square) {
+        case 'n':
+          coords.y += 10;
+          vertices.push([coords.x, coords.y]);
+        case 's':
+          coords.y -= 10;
+          vertices.push([coords.x, coords.y]);
+        case 'w':
+          coords.x += 10;
+          vertices.push([coords.x, coords.y]);
+        case 'e':
+          coords.x -= 10;
+          vertices.push([coords.x, coords.y]);
+      }
+    });
+
+    debugger;
+    setup(vertices);
+  });
+};
+
+
+function setup(vertices) {
+  noFill();
+  beginShape();
+  debugger;
+  vertices.forEach(v => vertex(...v));
+  endShape();
 }
 
-function draw() {
+// function setup(vertices) {
+//   noFill();
+//   beginShape();
+//   vertex(30, 20);
+//   vertex(85, 20);
+//   vertex(85, 75);
+//   vertex(30, 75);
+//   endShape();
+// }
 
-}
+// function draw(vertices) {
+//   // debugger;
+//   noFill();
+//   beginShape();
+//   vertices.forEach(v => vertex(...v));
+//   endShape();
+// }
 
 document.addEventListener("DOMContentLoaded", () => {
 
   window.setup = setup;
-  window.draw = draw;
+  // window.draw = draw;
+  // window.noFill = noFill;
+  // window.beginShape = beginShape;
+  // window.vertex = vertex;
+  //
 
   const grid = createGrid(5, 5);
   console.log(grid);
   carvePassagesFrom(0, 0, grid);
   console.log(grid);
+  drawMaze(grid);
 });
